465836c9d325b18e15723504aa431ec68974e978
Fayyaz Alam <fayazalam.98@gmail.com>
2021-05-21 20:09:39 -0700

    assignment 20 t0 25

---
+++ app.js
@@ -0,0 +1,28 @@
+// **********QUESTION 1**********
+
+// var firstName = prompt("Enter Your First Name Here")
+// var lastName = prompt("Enter Your Last Name Here")
+// var fullName = firstName + " " + lastName
+// var greetings = alert("Welcome!" + " " + fullName)
+
+
+// **********QUESTION 2**********
+
+// var input = prompt("Ask me Your Favourite Mobile Name")
+// document.write("<h3>" + "My Favourite Phone is: " + input + "<h3>")
+// document.write("<h3>" + "Length of string: " + input.length + "<h3>")
+
+
+// **********QUESTION 3**********
+
+// var string = "Pakistani"
+// document.write("<h3>" + "String: " + string + "<h3>")
+// document.write("<h3>" + "Index of 'n': " + string.indexOf("n") + "<h3/>")
+
+
+// **********QUESTION 4**********
+
+// var string = "Hello World"
+// document.write("<h3>" + "String: " + string + "<h3>")
+// document.write("<h3>" + "Index of 'l': " + string.indexOf("l",5) + "<h3/>")
+
