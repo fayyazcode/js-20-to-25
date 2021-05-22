6cfbf3f16dbd5fdf6fdab66555b06e9a5c5923ea
fayyazcode <fayazalam.98@gmail.com>
2021-05-22 11:57:19 +0500

    text changed

--- app.js
+++ app.js
@@ -1,30 +1,30 @@
 // **********QUESTION 1**********
 
-// var firstName = prompt("Enter Your First Name Here")
-// var lastName = prompt("Enter Your Last Name Here")
+// var firstName = prompt("Enter Your First Name ")
+// var lastName = prompt("Enter Your Last Name")
 // var fullName = firstName + " " + lastName
 // var greetings = alert("Welcome!" + " " + fullName)
 
 
 // **********QUESTION 2**********
 
-// var input = prompt("Ask me Your Favourite Mobile Name")
-// document.write("<h3>" + "My Favourite Phone is: " + input + "<h3>")
-// document.write("<h3>" + "Length of string: " + input.length + "<h3>")
+// var input = prompt("Tell me Your Favourite Mobile Name")
+// document.write("<h4>" + "My Favourite Phone is: " + input + "<h4>")
+// document.write("<h4>" + "Length of string: " + input.length + "<h4>")
 
 
 // **********QUESTION 3**********
 
 // var string = "Pakistani"
-// document.write("<h3>" + "String: " + string + "<h3>")
-// document.write("<h3>" + "Index of 'n': " + string.indexOf("n") + "<h3/>")
+// document.write("<h4>" + "String: " + string + "<h4>")
+// document.write("<h4>" + "Index of 'n': " + string.indexOf("n") + "<h4/>")
 
 
 // **********QUESTION 4**********
 
 // var string = "Hello World"
-// document.write("<h3>" + "String: " + string + "<h3>")
-// document.write("<h3>" + "Index of 'l': " + string.indexOf("l",5) + "<h3/>")
+// document.write("<h4>" + "String: " + string + "<h4>")
+// document.write("<h4>" + "Index of 'l': " + string.indexOf("l",5) + "<h4/>")
 
 //*****************extra***********
 // var name="fayyaz alam jan alam"
